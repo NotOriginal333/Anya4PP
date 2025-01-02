51f9c06ed21e5c6a1f066313ff715dc2638987f9
NotOriginal333 <m.naumovych2@gmail.com>
2025-01-02 20:55:20 +0200

    changed to sqlite db

--- lab4/lab4/settings.py
+++ lab4/lab4/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'lab4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Lab4Rita',
-        'USER': 'postgres',
-        'PASSWORD': 'axs22fg@',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
